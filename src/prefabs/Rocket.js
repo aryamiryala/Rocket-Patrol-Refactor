afc022f79579735a663f49e60f2aae2b01a9016d
Arya Miryala <aryamiryala5@gmail.com>
2023-04-18 19:51:24 -0700

    done with reset

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -34,5 +34,12 @@ class Rocket extends Phaser.GameObjects.Sprite {
             this.isFiring = false; 
             this.y = game.config.height - borderUISize - borderPadding; 
         }
+        
+    }
+    //reset rocket to ground
+    reset() {
+        this.isFiring = false; 
+        this.y = game.config.height - borderUISize - borderPadding; 
+
     }
 }
